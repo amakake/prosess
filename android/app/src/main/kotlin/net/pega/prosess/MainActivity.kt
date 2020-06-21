c3b0072614d44795851fb87186c073a4a4048a4a
kubotenshou <amakake120981@gmail.com>
2020-06-21 14:00:20 +0900

    first commit

---
+++ android/app/src/main/kotlin/net/pega/prosess/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.pega.prosess
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:prosess/main_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+
+            title: Text("prosess"),
+          ),
+          body: Consumer<MainModel>(
+            builder: (context, model,child) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      model.amakakeText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        "ボタン"
+                      ),
+                      onPressed: () {
+                        //TODO
+                        model.ChangeamakakeText();
+                      },
+                    )
+                  ],
+                ),
+              );
+            }
+          ),
+        ),
+      ),
+    );
+  }
+}
